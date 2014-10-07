60a04cabc278f865ac811d14e2802faa8753b476
U San <properpig@gmail.com>
2014-10-08 04:00:59 +0800

    added view for liked food

--- food/urls.py
+++ food/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
   # ex: /orders/
   url(r'^test/', views.test, name='test'),
   url(r'^foodlist/(?P<username>\w+)/', views.FoodListView, name='foodlist'),
+  url(r'^likedfoodlist/(?P<username>\w+)/', views.LikedFoodListView, name='likedfoodlist'),
 )
--- food/views.py
+++ food/views.py
@@ -39,3 +39,30 @@ def FoodListView(request, username):
         food_list_serialized.append(food_obj)
 
     return HttpResponse(json.dumps(food_list_serialized), content_type="application/json")
+
+@csrf_exempt
+def LikedFoodListView(request, username):
+
+    user = User.objects.get(username=username)
+
+    food_list = user.foods_liked.all()
+    food_list_serialized = []
+
+    for food in food_list:
+
+        food_obj = {}
+        food_obj['name'] = food.name
+        food_obj['description'] = food.description
+        food_obj['price'] = '${0:0.2f}'.format(food.price)
+
+        food_obj['photo'] = food.photo
+        food_obj['is_halal'] = food.is_halal
+        food_obj['is_vegan'] = food.is_vegan
+        food_obj['cuisine'] = food.cuisine
+        food_obj['restaurant'] = food.restaurant.name
+
+        food_obj['num_likes'] = User.objects.filter(foods_liked__in=[food]).count()
+
+        food_list_serialized.append(food_obj)
+
+    return HttpResponse(json.dumps(food_list_serialized), content_type="application/json")
